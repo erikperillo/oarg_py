a61cc9776e679f4416e9a937cdf8127a2a3a04cb
Erik Perillo <erik.perillo@gmail.com>
2015-05-27 01:57:11 -0300

    new way for parsing

--- oarg.py
+++ oarg.py
@@ -1,146 +1,110 @@
 import sys
+import re
 
 class Oarg:
-     invalid_options = []
-     def __init__(self, tp, names, def_val, description, pos_n_found=0):
-          self.tp             = tp
-          self.names          = list(names.split())
-          self.def_val        = def_val
-          self.description    = description
-          self.pos_n_found    = pos_n_found
-          self.found          = False
-          self.str_vals       = []
-          self.vals           = None
-          for i in range(len(self.names)):
-               self.names[i] = Oarg.pureName(self.names[i])
-          Container.add(self)
+    invalid_options = []
+    def __init__(self, tp, names, def_val, description, pos_n_found=-1):
+        self.tp           = tp
+        self.names        = [Oarg.pureName(n) for n in (names.split() if type(names) == str else names)]
+        self.def_val      = def_val
+        self.description  = description
+        self.pos_n_found  = pos_n_found
+        self.found        = False
+        self.str_vals     = []
+        self.vals         = (def_val,)
+        Container.add(self)
 
-     def getVal(self,index=0):
-          if index >= 0 and index < len(self.str_vals):
-               return self.vals[index]
-          if self.tp is bool and self.found:
-               return not self.def_val
-          return self.def_val
+    @property
+    def val(self):
+        return self.vals[0]    
 
-     def getTupleVal(self):
-          return self.vals
+    def setVals(self,str_vals,falses):
+        self.str_vals = str_vals
+        self.found = True
+        if self.tp == bool:
+            self.vals = tuple( not i.lower() in falses for i in self.str_vals ) if self.str_vals != [] else (not self.def_val,)
+        else:
+            self.vals = tuple( self.tp(i) for i in self.str_vals ) if self.str_vals != [] else (self.def_val,)
+        
+    @staticmethod
+    def pureName(name):
+        while name[0] == "-":
+            name = name[1:]
+        return name
 
-     def wasFound(self):
-          return self.found
+    @staticmethod
+    def clName(name):
+        insertion = "-"
+        return insertion + name
 
-     def setTuple(self):
-          if len(self.str_vals) == 0:
-               if self.tp == bool and self.found:
-                    self.vals = (not self.def_val,) 
-               else:
-                    self.vals = (self.def_val,)
-               return
-          if self.tp == bool:
-               self.vals = tuple( i in ["True","true","TRUE","y","yes","Y","Yes","YES","1"] for i in self.str_vals )
-          else:
-               self.vals = tuple(self.tp(i) for i in self.str_vals)
-          
-     @staticmethod
-     def pureName(name):
-          while name[0] == "-":
-               name = name[1:]
-          while name[len(name)-1] == ":":
-               name = name[:(len(name)-1)]
-          return name
-
-     @staticmethod
-     def clName(name):
-          insertion = "-"
-          if len(name) > 1:
-               insertion += "-"
-          return insertion + name
-
-     @staticmethod
-     def isClName(name):
-          if len(name) < 2:
-               return name[0] == "-"
-          return name[0] == "-" and (ord(name[1]) < 48 or ord(name[1]) > 57) 
-          
+    @staticmethod
+    def isClName(name):
+        return (name[0] == "-" and (ord(name[1]) < 48 or ord(name[1]) > 57)) if len(name) > 1 else False
+        
 class Container:
-     oargs = []
-     @staticmethod
-     def add(oarg):
-          Container.oargs.append(oarg)
+    oargs = []
+    @staticmethod
+    def add(oarg):
+        Container.oargs.append(oarg)
 
-def osplit(string,delim=","):
-     ret = []
-     i = 0
-     while i < len(string):
-          if string[i] == delim:
-               if i > 0 and string[i-1] == "\\":
-                    string = string[:(i-1)] + string[i:]
-                    i -= 1
-               else:
-                    if string[:i] != "":
-                         ret.append(string[:i])
-                    string = string[(i+1):]
-                    i = -1
-          i += 1
-     if string != "":
-          ret.append(string)
-     return ret
+def parse(source=sys.argv,falses=["false","no","n","0"]):
+    _src = list(source)[1:] #assuming first argument is program's name
+    
+    for i,elem in enumerate(_src):
+        _elem = re.sub(r'(?<!\\),',',,',elem)
+        _src[i] = re.sub(r'\\,',',',_elem)
 
-def parse(argv=sys.argv):
-     #list for oargs with pos_n_found
-     argv = list(argv) #TODO: remove
-     pos_vec = []
-     #main loop
-     for oarg in Container.oargs:
-          mark = None
-          deleted = False
-          for i,arg in enumerate(argv):
-               if not oarg.found:
-                    for name in oarg.names:
-                         if arg == Oarg.clName(name):
-                              oarg.found = True
-                              mark = arg
-                              break
-               else:
-                    if not deleted:
-                         if not (oarg.tp == bool and Oarg.isClName(arg)):
-                             oarg.str_vals = osplit(arg)
-                             argv.remove(arg)
-                         argv.remove(argv[i-1])
-                         deleted = True
-                         break
-                    else:
-                         continue
-          if oarg.found and not deleted:
-               argv.remove(mark)
-               deleted = True
-          #appendind to list of pos_n_found if set
-          if not oarg.found and oarg.pos_n_found > 0:
-               pos_vec.append(oarg)
-          #setting tuple of values
-          oarg.setTuple()
-     #sorting in ascending order
-     #checking validity of arguments
-     for arg in argv:
-          if(Oarg.isClName(arg)):
-               Oarg.invalid_options.append(arg)
-     pos_vec.sort(key=lambda x: x.pos_n_found)               
-     #getting not read values from cl
-     for oarg in pos_vec:
-          if len(argv) > 1 and not Oarg.isClName(argv[1]): 
-               oarg.str_vals = osplit(argv[1])
-               oarg.found = True
-               argv.remove(argv[1])
-               oarg.setTuple()
-     #returns number of unknown parameters
-     return len(Oarg.invalid_options)
+    src = " " + ",".join(_src)
+    while src.find(",,,") >= 0:
+        src = src.replace(",,,",",,")
+    src = re.split(r'[ ,]-(?![0-9])',src)
+
+    oargs_dict = dict( (Oarg.pureName(key),val) for val in Container.oargs for key in val.names )
+
+    remaining = []
+    for __cand in src:
+        _cand = __cand.split(",,")
+        cand = _cand[0]
+        remaining_partial = _cand[1:]
+        cands = cand.split(",")
+
+        if ("-" + cands[0]) in sys.argv:
+            opt = cands[0]
+            args = cands[1:]
+            #print "\topt:",opt
+            #print "\targs:",args
+            if opt in oargs_dict:
+                oarg = oargs_dict[opt]
+                oarg.setVals(args,falses)
+            else:
+                Oarg.invalid_options.append(opt)
+        else:
+            remaining_partial = [_cand[0]] + remaining_partial
+
+        remaining += remaining_partial
+
+    remaining = [ i for i in remaining if i != "" ]
+
+    left_oargs = [ o for o in Container.oargs if not o.found and o.pos_n_found >= 0 ]
+    if left_oargs != []: 
+        left_oargs.sort(key=lambda x: x.pos_n_found)
+
+    while len(remaining) > 0 and len(left_oargs) > 0:
+        oarg = left_oargs[0]    
+        oarg.setVals(remaining[0].split(","),falses)
+        remaining = remaining[1:]
+        left_oargs = left_oargs[1:]
+
+    return len(Oarg.invalid_options)
+
 
 def describeArgs(helpmsg=""):
-     if helpmsg != "":
-          print helpmsg
-     for oarg in Container.oargs:
-          names = ""
-          for i in range(len(oarg.names)-1):
-               names += Oarg.clName(oarg.names[i]) + ", "
-          names += Oarg.clName(oarg.names[len(oarg.names)-1])
-          print "{0:48}{1}".format(names,oarg.description)
-          #print names,"\t\t\t\t",oarg.description
+    if helpmsg != "":
+        print helpmsg
+    for oarg in Container.oargs:
+        names = ""
+        for i in range(len(oarg.names)-1):
+            names += Oarg.clName(oarg.names[i]) + ", "
+        names += Oarg.clName(oarg.names[len(oarg.names)-1])
+        print "{0:48}{1}".format(names,oarg.description)
+        #print names,"\t\t\t\t",oarg.description
--- test.py
+++ test.py
@@ -1,3 +1,44 @@
+import sys
+import re
+
+
+res = list(sys.argv[1:])
+
+for k,i in enumerate(res):
+    _i = re.sub(r'(?<!\\),',',,',i)
+    res[k] = re.sub(r'\\,',',',_i)
+
+res = " " + ",".join(res)
+for delim in [",,,"]:
+    while res.find(delim) >= 0:
+        res = res.replace(delim,",,")
+
+print res
+
+res = res.replace(" -",",-").split(",-")
+print
+print res
+
+remaining = []
+for __cand in res:
+    _cand = __cand.split(",,")
+    cand = _cand[0]
+    _remaining = _cand[1:]
+    cands = cand.split(",")
+
+    if ("-" + cands[0]) in sys.argv:
+        opt = cands[0]
+        args = cands[1:]
+        print "\topt:",opt
+        print "\targs:",args
+    else:
+        _remaining = [_cand[0]] + _remaining
+
+    remaining += _remaining
+
+    print "\tremaining:",remaining
+exit()
+
 from oarg import *
 
 ti = Oarg(int,"-i --int-val",-2,"Integer value",2)
